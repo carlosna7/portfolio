e6f214178c964b84070e2220fe4f44fd98eadbe1
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-06-06 17:43:46 -0300

    att

---
+++ script/script2.js
@@ -0,0 +1,21 @@
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.9);
+    target.forEach(function(element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+
+        console.log(element.offsetTop)
+    })
+}
+
+if(target.length) {
+    window.addEventListener("scroll", function() {
+        animeScroll();
+    })
+}
