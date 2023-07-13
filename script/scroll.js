29bbb0df8dd81cb0747de3796451a102c5d8eee9
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-07-13 11:25:24 -0300

    att

---
+++ script/scroll.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    const left = document.querySelector(".scroll-images");
+    left.scrollBy(-340, 0);
+}
+
+function scrollr() {
+    const right = document.querySelector(".scroll-images");
+    right.scrollBy(340, 0);
+}
